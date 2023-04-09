bb70504d8ff7eabed57c5d60266496e00a6fe20e
Alim <alimabbas44891@gmail.com>
2023-04-09 08:10:35 +0530

    ChatBOT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+import { useState } from "react";
+
+const API_KEY = "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";
 
 function App() {
+  const [typing, setTyping] = useState(false);
+  const [persona, setPersona] = useState("human");
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT",
+      sender: "ChatGPT",
+    },
+  ]);
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
+    };
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+
+    setTyping(true);
+
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if (messageObject.sender === "ChatGPT") {
+        role = "assistant";
+      } else {
+        role = "user";
+      }
+      return { role: role, content: messageObject.message };
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: toString(persona),
+    };
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [systemMessage, ...apiMessages],
+    };
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMessages([
+          ...chatMessages,
+          {
+            message: data.choices[0].message.content,
+            sender: "ChatGPT",
+          },
+        ]);
+        setTyping(false);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          style={{
+            border: "none",
+            backgroundColor: "skyblue",
+            padding: "2px 10px",
+            borderRadius: "10px",
+            width: "600px",
+            outline: "none",
+            height: "40px",
+            margin: "20px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          placeholder="Add your persona here "
+          onChange={(e) => setPersona(e.target.value)}
+        />
+      </div>
+      <div
+        style={{
+          display: "inline-block",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "800px",
+          width: "600px",
+          color: "red",
+        }}
+      >
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior="smooth"
+              typingIndicator={
+                typing ? <TypingIndicator content="ChatGPT is typing" /> : null
+              }
+            >
+              {messages?.map((message, i) => {
+                return <Message key={i} model={message} />;
+              })}
+            </MessageList>
+            <MessageInput
+              placeholder="Type your message here"
+              onSend={handleSend}
+            />
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   );
 }
